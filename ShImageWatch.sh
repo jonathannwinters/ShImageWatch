5ab9d79b30a79b08e14e23991c8da22251e146dd
jonathannwinters <jonathan@winters.im>
2017-07-21 23:38:13 -0400

    Update ShImageWatch.sh

--- ShImageWatch.sh
+++ ShImageWatch.sh
@@ -1,46 +1,52 @@
 #!/bin/sh
 
-#This is a Work In Progress
 
-#Bash script Image Watcher
-
-###########
-# Title: biw.sh
-# Descripton: A linux shell script that monitors a folder for new images and displays them as they are added.
-# Author: Jonathan N. Winters, Jonathan@Winters.im
+############################################################################
+# Title: ShImageWatch.sh                                                   
+#                                                                          
+# Descripton: A linux shell script that monitors a folder for new images   
+# and displays them as they are added.                                     
+#                                                               
+# Author: Jonathan N. Winters, jnw25@cornell.edu                           
 # Date:   July 19, 2017
-# Version: 0.1
+# Version: 0.2
 #
 # Requirements / Dependencies
 # Install  inotify-tools pakage
 #     $sudo apt-get install inotify-tools
 # Install  imagemagick
 #     $sudo apt-get install imagemagick
-#
+############################################################################
+
 
 #resources http://www.imagemagick.org/Usage/anim_basics/
 
 # set your parameters
-path_to_app="/REPLACE/WITH/FULL/PATH/TO/THIS/APP/DIRECTORY"  #next iteration use ${BASH_SOURCE[0]}
-path_to_images="/REPLACE/WITH/FULL/PATH/TO/IMAGES/DIRECTORY"
-path_to_splash="/REPLACE/WITH/FULL/PATH/TO/SPLASH/IMAGE/FILE"
+expected_image_type=".TYPE" #replace the string .TYPE value with .jpg, .gif, or .png
+
+path_to_this_app="/REPLACE/WITH/FULL/PATH/TO/THIS/APP/DIRECTORY/"
+path_to_new_images="/REPLACE/WITH/FULL/PATH/TO/LOCATION/OF/INCOMING/IMAGES/"
+path_to_past_images="/REPLACE/WITH/FULL/PATH/TO/SPLASH/IMAGE/FILE/"
+
+
+# sets the path to splash based on the location of this folder
+path_to_splash="${path_to_this_app}splash${expected_image_type}"
 
 # generate the path that will be used as the currently displayed file
-path_to_current_image="${path_to_app}/current"
+# note the filename becomes "current" without any extension, this is to handle any file type
+path_to_current_image="${path_to_this_app}current${expected_image_type}"
 
-# Copy the  splash screen image to the current image location
-cp ${path_to_splash} ${path_to_current_image}
+# Copy the splash screen image to the current image location
+cp $path_to_splash $path_to_current_image
+
+#display the splash "current" image
 display -update 1 $path_to_current_image &
 
-inotifywait -m $path_to_images -e create -e moved_to | 
+inotifywait -m $path_to_new_images -e create -e moved_to | 
     while read path action file; do
-
-        #rename new image to current.jpg
-        echo "HelloWorld $file"
-        #rm $path_to_current_image
-        cp -f $path$file ${path}displayed${file}
+        cp -f $path$file ${path_to_past_images}${file}
         mv -f $path$file $path_to_current_image
-        echo $path$file $path_to_current_image
+        #echo $path$file $path_to_current_image
     done
 
 
