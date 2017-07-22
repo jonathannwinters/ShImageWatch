59fd74f84e7b8149c3374ef8a73693417355a135
jonathannwinters <jonathan@winters.im>
2017-07-22 00:04:11 -0400

    adding additional comments

--- ShImageWatch.sh
+++ ShImageWatch.sh
@@ -26,7 +26,7 @@ path_to_new_images="/REPLACE/WITH/FULL/PATH/TO/LOCATION/OF/INCOMING/IMAGES/"
 path_to_past_images="/REPLACE/WITH/FULL/PATH/TO/DESTINATION/DIRECTORY/FOR/PAST/IMAGES"
 
 
-# sets the path to splash based on the location of this folder
+# sets the path to splash based on the location of this directory
 path_to_splash="${path_to_this_app}splash${expected_image_type}"
 
 # generate the path that will be used as the currently displayed file
@@ -36,13 +36,18 @@ path_to_current_image="${path_to_this_app}current${expected_image_type}"
 # Copy the splash screen image to the current image location
 cp $path_to_splash $path_to_current_image
 
-#display the splash "current" image
+# display the splash "current" image and set ImageMagick to continuously update
 display -update 1 $path_to_current_image &
 
+#monitor the new images directory for incoming new images
 inotifywait -m $path_to_new_images -e create -e moved_to | 
     while read path action file; do
+        #duplicate the incoming image into the past images directory
         cp -f $path$file ${path_to_past_images}${file}
+        # set the new image to the current image so ImageMagick can update the display
         mv -f $path$file $path_to_current_image
+
+        #print a confirmation to the concole that the image has been processed 
         timestamp=`date "+%Y-%m-%d %H:%M:%S"`
         echo "$timestamp  $file"
     done
